564a6ea9c95365af95d5fad7915175de2a15d20b
hiXgb <xieguobiyi@gmail.com>
2015-07-22 13:47:37 +0800

    hello

--- hello.py
+++ hello.py
@@ -1,22 +1,3 @@
-import os  
-
-def function():
-	print('aa')
-	for x in xrange(1,10):
-		print(x)
-		if x == 5:
-			return
-
-
-
-function()
-
-
-for dirName, subdirList, fileList in os.walk(rootDir):  
-    	for fname in fileList: 
-     		path = '%s/%s' % (dirName,fname)
-     		if subprocess.call("grep \"%s\" %s" %(keyWord,path), shell=True) == 0:
-          		print('%s is used!!! path is %s' % (keyWord,path))
-          		return
+print('hello world!')
 
 
